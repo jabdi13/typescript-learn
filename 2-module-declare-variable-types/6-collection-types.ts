f48b4f993830463e60c1dc8406cba61a3888ec49
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-11 16:43:45 -0600

    Collection types

---
+++ 2-module-declare-variable-types/6-collection-types.ts
@@ -0,0 +1,11 @@
+// Arrays
+let list: number[] = [1, 2, 3];
+// Other option
+// let list: Array<number> = [1, 2, 3];
+
+// Tuples
+let person1: [string, number] = ['Marcia', 35];
+
+// let person1: [string, number] = ['Marcia', 35, true]; // Wrong assignment
+
+// let person1: [string, number] = [35, 'Marcia']; // Wrong assignment
