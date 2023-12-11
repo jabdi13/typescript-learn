158d181c9a1e63f1a6f06add41d8e8d99d892909
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-11 14:53:15 -0600

    Enums

---
+++ 2-module-declare-variable-types/3-enums.ts
@@ -0,0 +1,9 @@
+enum ContractStatus {
+  Permanent,
+  Temp,
+  Apprentice
+}
+
+let employeeStatus: ContractStatus = ContractStatus.Temp;
+console.log(employeeStatus);
+console.log(ContractStatus[employeeStatus]);
