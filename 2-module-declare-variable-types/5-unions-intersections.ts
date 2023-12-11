4b21ce2224c18ef6a77508ecdeb4677efede2dc4
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-11 16:34:49 -0600

    Unions intersections and literals

---
+++ 2-module-declare-variable-types/5-unions-intersections.ts
@@ -0,0 +1,46 @@
+// Union types
+let multiType: number | boolean;
+multiType = 20;         //* Valid
+multiType = true;       //* Valid
+multiType = "twenty";   //* Invalid
+
+function add(x: number | string, y: number | string) {
+  if (typeof x === 'number' && typeof y === 'number') {
+      return x + y;
+  }
+  if (typeof x === 'string' && typeof y === 'string') {
+      return x.concat(y);
+  }
+  throw new Error('Parameters must be numbers or strings');
+}
+console.log(add('one', 'two'));  //* Returns "onetwo"
+console.log(add(1, 2));          //* Returns 3
+console.log(add('one', 2));      //* Returns error
+
+// Intersection types
+interface Employee {
+  employeeID: number;
+  age: number;
+}
+interface Manager {
+  stockPlan: boolean;
+}
+type ManagementEmployee = Employee & Manager;
+let newManager: ManagementEmployee = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true
+};
+
+// Literal types
+type testResult = "pass" | "fail" | "incomplete";
+let myResult: testResult;
+myResult = "incomplete";    //* Valid
+myResult = "pass";          //* Valid
+myResult = "failure";       //* Invalid
+
+type dice = 1 | 2 | 3 | 4 | 5 | 6;
+let diceRoll: dice;
+diceRoll = 1;    //* Valid
+diceRoll = 2;    //* Valid
+diceRoll = 7;    //* Invalid
