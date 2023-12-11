1e1883df47b7693d3876db54ff966e16ec3c2e50
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-11 14:48:48 -0600

    Primitive types

---
+++ 2-module-declare-variable-types/2-primitive-types.ts
@@ -0,0 +1,20 @@
+// Boolean type
+let flag: boolean;
+let yes = true;
+let no = false;
+
+// Number type
+let x: number;
+let y = 0;
+let z: number = 123.456;
+let big: bigint = 100n;
+
+// String type
+let s: string;
+let empty = "";
+let abc = 'abc';
+
+let firstName: string = "Mateo";
+let sentence: string = `My name is ${firstName}.
+    I am new to TypeScript.`;
+console.log(sentence);
